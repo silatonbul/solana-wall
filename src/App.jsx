d9ff515ce34eadf5c737871fd19c189424bbe935
silatonbul <80275818+silatonbul@users.noreply.github.com>
2023-03-05 16:17:15 +0300

    building the photo grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
+const TEST_GIFS = [
+	'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
+
 // Constants
 const TWITTER_HANDLE = 'silatonbul_';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
-  const [walletAddress, setWalletAddress] = useState(null);
+const [walletAddress, setWalletAddress] = useState(null);
+const [inputValue, setInputValue] = useState('');
+const [gifList, setGifList] = useState([]);
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
@@ -48,6 +57,21 @@ const App = () => {
   }
 };
 
+const sendGif = async () => {
+  if (inputValue.length > 0) {
+    console.log('Gif link:', inputValue);
+    setGifList([...gifList, inputValue]);
+    setInputValue('');
+  } else {
+    console.log('Input can not be empty please try again');
+  }
+};
+
+  const onInputChange = (event) => {
+  const { value } = event.target;
+  setInputValue(value);
+  };
+
   /*
    * We want to render this UI when the user hasn't connected
    * their wallet to our app yet.
@@ -60,6 +84,29 @@ const App = () => {
       Connect to Wallet
     </button>
   );
+
+const renderConnectedContainer = () => (
+  <div className="connected-container">
+    {/* Go ahead and add this input and button to start */}
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        sendGif();
+      }}
+    >
+      <input type="text" placeholder="Enter your photo link!" 
+        value={inputValue} onChange={onInputChange}/>
+      <button type="submit" className="cta-button submit-gif-button">Submit</button>
+    </form>
+    <div className="gif-grid">
+      {gifList.map((gif) => (
+        <div className="gif-item" key={gif}>
+          <img src={gif} alt={gif} />
+        </div>
+      ))}
+    </div>
+  </div>
+);
   
   useEffect(() => {
     const onLoad = async () => {
@@ -68,6 +115,17 @@ const App = () => {
     window.addEventListener('load', onLoad);
     return () => window.removeEventListener('load', onLoad);
   }, []);
+
+  useEffect(() => {
+  if (walletAddress) {
+    console.log('Fetching Photo list...');
+    
+    // Call Solana program here.
+
+    // Set state
+    setGifList(TEST_GIFS);
+  }
+}, [walletAddress]);
 
   return (
     <div className="App">
@@ -78,6 +136,7 @@ const App = () => {
             Protect memories with your personal Solana Wall!
           </p>
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
