d60fcca24ec436773f8d76712144e154b665a668
silatonbul <80275818+silatonbul@users.noreply.github.com>
2023-03-05 15:39:42 +0300

    added connect phantom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'silatonbul_';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  /*
+   * This function holds the logic for deciding if a Phantom Wallet is
+   * connected or not
+   */
+  const checkIfWalletIsConnected = async () => {
+  // We're using optional chaining (question mark) to check if the object is null
+    if (window?.solana?.isPhantom) {
+    console.log('Phantom wallet found!');
+    /*
+    * The solana object gives us a function that will allow us to connect
+    * directly with the user's wallet
+    */
+    const response = await window.solana.connect({ onlyIfTrusted: true });
+    console.log(
+      'Connected with Public Key:',
+      response.publicKey.toString()
+    );
+    } else {
+      alert(' 👻 You need Phantom Wallet! 👻 ');
+    }
+  };
+
+  /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  const connectWallet = async () => {};
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+  
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Solana Wall</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            Protect memories with your personal Solana Wall!
           </p>
         </div>
         <div className="footer-container">
@@ -23,7 +72,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
